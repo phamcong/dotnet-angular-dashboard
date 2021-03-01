97d958e0a2b00cfe4537eb80fc396b2307cd485c
phamcu <ccuong.ph@gmail.com>
2021-03-01 01:11:14 +0000

    setup routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { SectionHealthComponent } from './sections/section-health/section-health.component'
+import { SectionOrdersComponent } from './sections/section-orders/section-orders.component'
+import { SectionSalesComponent } from './sections/section-sales/section-sales.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sales', component: SectionSalesComponent },
+  { path: 'orders', component: SectionOrdersComponent },
+  { path: 'health', component: SectionHealthComponent },
+  { path: '', redirectTo: '/sales', pathMatch: 'full' }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { NavbarComponent } from './navbar/navbar.component'
+import { SectionHealthComponent } from './sections/section-health/section-health.component'
+import { SectionOrdersComponent } from './sections/section-orders/section-orders.component'
+import { SectionSalesComponent } from './sections/section-sales/section-sales.component'
+import { SidebarComponent } from './sidebar/sidebar.component'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    SectionSalesComponent,
+    SectionOrdersComponent,
+    SectionHealthComponent
   ],
   imports: [
     BrowserModule,
