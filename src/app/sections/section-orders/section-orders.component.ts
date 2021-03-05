b594f3fa1a1c9c490472cc22a0a335e928baa5d8
phamcu <ccuong.ph@gmail.com>
2021-03-05 20:04:34 +0000

    add section orders data

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Order } from 'src/app/shared/order'
 
 @Component({
   selector: 'app-section-orders',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section-orders.component.scss']
 })
 export class SectionOrdersComponent implements OnInit {
+  public orders: Order[] = [...Array(5)].map(item => (
+    {
+      id: 1,
+      customer: { id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com' },
+      total: 230, placed: new Date(2020, 1, 1),
+      fulfilled: new Date(2020, 2, 1),
+      status: 'Completed'
+    }
+  ))
 
   constructor() { }
 
